4c5c4554ac4f6b0573e34507e12e064497beb371
why520crazy <why520crazy@163.com>
2020-02-24 17:28:19 +0800

    perf: improve perf

---
+++ .versionrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  bumpFiles: [
+    {
+      filename: "src/MY_VERSION_TRACKER.txt",
+      type: "plain-text"
+    },
+    {
+      filename: "src/package.json",
+      type: "json"
+    },
+    {
+      filename: "package.json",
+      type: "json"
+    },
+    {
+      filename: "src/VERSION_TRACKER.json",
+      updater: "./scripts/standard-version-updater.js"
+    }
+  ],
+  preset: require.resolve("conventional-changelog-angular")
+};
